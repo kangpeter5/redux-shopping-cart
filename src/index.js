c15d50007518b892f47ad47f89e2f1aeab2abf84
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-05 13:51:33 -0700

    adding items

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import App from './App';
 import './index.css';
 
+// chrome redux devtools
+/* eslint-disable no-underscore-dangle */
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-enable */
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  	<App />
+  </Provider>,
   document.getElementById('root')
 );
